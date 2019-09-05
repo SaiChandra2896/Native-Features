1aabbbb471f75aac8927bf9aed547cd85d461940
SaiChandra2896 <saichandra2896@gmail.com>
2019-09-05 15:09:13 +0530

    basic screens and navigation setup

--- App.js
+++ App.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import PlacesNavigator from './navigation/PlacesNavigator';
 
 export default function App() {
-  return (
-    <View>
-      <Text>hy</Text>
-    </View>
-  );
+  return <PlacesNavigator />;
 }

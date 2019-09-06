028a0e693c2029af7a4819e3fc2109f7465764ed
SaiChandra2896 <saichandra2896@gmail.com>
2019-09-06 15:36:13 +0530

    set up redux to add new place

--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -7,16 +7,24 @@ import {
   ScrollView,
   Button
 } from 'react-native';
+import { useDispatch } from 'react-redux';
+
 import Colors from '../constants/Colors';
+import * as placesActions from '../store/places-actions';
 
 const NewPlaceScreen = props => {
   const [titleValue, setTitleValue] = useState('');
+
+  const dispatch = useDispatch();
 
   const titleChangeHandler = text => {
     setTitleValue(text);
   };
 
-  const savePlaceHandler = () => {};
+  const savePlaceHandler = () => {
+    dispatch(placesActions.addPlace(titleValue));
+    props.navigation.goBack();
+  };
 
   return (
     <ScrollView>

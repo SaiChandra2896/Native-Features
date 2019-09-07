efec1ed8b2c89d95f856cf5cf2de3cf280f19160
SaiChandra2896 <saichandra2896@gmail.com>
2019-09-07 14:50:40 +0530

    add queries to initialize sql db

--- App.js
+++ App.js
@@ -5,6 +5,15 @@ import ReduxThunk from 'redux-thunk';
 
 import PlacesNavigator from './navigation/PlacesNavigator';
 import placesReducer from './store/places-reducer';
+import { init } from './helpers/db';
+
+init()
+  .then(() => {
+    console.log('initialized database');
+  })
+  .catch(err => {
+    console.log('db failed', err);
+  });
 
 const rootReducer = combineReducers({
   places: placesReducer
---
+++ helpers/db.js
@@ -0,0 +1,23 @@
+import { SQLite } from 'expo-sqlite';
+
+const db = SQLite.openDatabase('places.db');
+
+export const init = () => {
+  const promise = new Promise((resolve, reject) => {
+    db.transaction(tx => {
+      tx.executeSql(
+        'CREATE TABLE IF NOT EXISTS places (id INTEGER PRIMARY KEY NOT NULL, title TEXT NOT NULL, imageUri TEXT NULL, address TEXT NOT NULL, lat REAL NOT NULL, lng REAL NOT NULL);',
+        [],
+        //SUCCESS FUNCTION
+        () => {
+          resolve();
+        },
+        //ERROR FUNCTION
+        (_, err) => {
+          reject(err);
+        }
+      );
+    });
+  });
+  return promise;
+};
